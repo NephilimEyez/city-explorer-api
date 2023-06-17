87636e9914b96b64118ab1f3630a414948726446
Gerald Bruno <34822464+NephilimEyez@users.noreply.github.com>
2023-06-17 12:21:22 -0400

    All working with cache

--- modules/movies.js
+++ modules/movies.js
@@ -2,22 +2,38 @@
 
 const axios = require('axios');
 
+let cache = require('./cache');
+
 async function getMovies(request, response, next) {
     try {
 
         let searchQuery = request.query.searchQuery;
         let movieURL = `https://api.themoviedb.org/3/search/movie?query=${searchQuery}&api_key=${process.env.MOVIE_API_KEY}&include_adult=false&language=en-US&page=1`;
 
-        let axiosData = await axios.get(movieURL);
-        let movieData = axiosData.data;
+        const key = `${searchQuery}--Movies`;
 
-        if (movieData) {
-
-            let movieRoll = movieData.results.map(film => new Movie(film));
-            response.status(200).send(movieRoll);
+        if (cache[key] && (Date.now() - cache[key].timestamp) < 10000) {
+            console.log('Cache was hit!', cache);
+      
+            response.status(200).send(cache[key].data);
+      
         } else {
-            response.status(404).send('No movies for that city.')
-        }
+            console.log('No item in cache');
+
+            let axiosData = await axios.get(movieURL);
+            let movieData = axiosData.data;
+
+                if (movieData) {
+
+                    let movieRoll = movieData.results.map(film => new Movie(film));
+                    cache[key] = {
+                        timestamp: Date.now(),
+                        data: movieRoll
+                    };
+                    response.status(200).send(movieRoll);
+                } else {
+                    response.status(404).send('No movies for that city.');
+        }}
     } catch (error) {
         response.status(500).send(error.message);
         next(error);
